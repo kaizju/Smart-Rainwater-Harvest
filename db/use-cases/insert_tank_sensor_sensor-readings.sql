f01886a9aed923addbb5146c2fa95f63113f7698
kaizju <rookieslit@gmail.com>
2026-03-08 14:25:27 +0800

    changes in insertion

--- db/use-cases/insert_tank_sensor_sensor-readings.sql
+++ db/use-cases/insert_tank_sensor_sensor-readings.sql
@@ -1,19 +1,18 @@
 START TRANSACTION;
 
-insert into users (username) values ('jude');
+INSERT INTO users (username)
+VALUES ('jude');
 
-insert into user_activity_logs (user_id, action)
-values (LAST_INSERT_ID(), 'put status of the tank');
+INSERT INTO user_activity_logs (user_id, action)
+VALUES (LAST_INSERT_ID(), 'put status of the tank');
 
-insert into tank (tankname,location_add,capacity,status_tank) 
-values ('Tank 1','Jude Crib','3,542L','Active');
+INSERT INTO tank (tankname, location_add, capacity, status_tank)
+VALUES ('Tank 1', 'Jude Crib', '3,542L', 'Active');
 
-insert into sensors (tank_id,sensor_type,model,unit,is_active) 
-values (1,'Water Level','Model 1',1,'Active');
+INSERT INTO sensors (tank_id, sensor_type, model, unit, is_active)
+VALUES (LAST_INSERT_ID(), 'Water Level', 'Model 1', '1', 'Active');
 
-insert into sensors_readings (sensor_id,user_id,recorded_at,anomaly) 
-values (1,1,CURRENT_TIMESTAMP,'None');
-
-
+INSERT INTO sensor_readings (sensor_id, user_id, anomaly)
+VALUES (LAST_INSERT_ID(), 1, 'None');
 
 COMMIT;
