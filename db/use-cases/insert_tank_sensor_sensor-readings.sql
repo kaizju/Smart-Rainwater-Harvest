7f15a7759ae21feda20608b4aab5996a5e9d7c7f
kaizju <rookieslit@gmail.com>
2026-03-08 14:11:41 +0800

    added insertion for sensor readings

--- db/use-cases/insert_tank_sensor_sensor-readings.sql
+++ db/use-cases/insert_tank_sensor_sensor-readings.sql
@@ -11,7 +11,8 @@ values ('Tank 1','Jude Crib','3,542L','Active');
 insert into sensors (tank_id,sensor_type,model,unit,is_active) 
 values (1,'Water Level','Model 1',1,'Active');
 
-
+insert into sensors_readings (sensor_id,user_id,recorded_at,anomaly) 
+values (1,1,CURRENT_TIMESTAMP,'None');
 
 
 
